6c63e5bbf2707b2fcb051fa58b35c743e3819998
ionSurf <javier.accounts@fiml.co>
2024-05-02 15:46:43 -0400

    feat: integrate i18n in BarChart component

--- plugins/time-saver/src/components/BarChartComponent/BarChartComponent.tsx
+++ plugins/time-saver/src/components/BarChartComponent/BarChartComponent.tsx
@@ -28,6 +28,11 @@ import { configApiRef, useApi } from '@backstage/core-plugin-api';
 import { fetchWithCredentials, getRandomColor } from '../utils';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { useTheme } from '@material-ui/core';
+//  Translation
+import { useTranslationRef } from '@backstage/core-plugin-api/alpha';
+import { TimeSaverTranslationRef } from '../../translationRef';
+
+const { t } = useTranslationRef(TimeSaverTranslationRef);
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip);
 
@@ -121,7 +126,7 @@ export function BarChart({
     labels,
     datasets: [
       {
-        label: 'Time Saved',
+        label: t('BarChart.dataAll.datasets.label'),
         data: datasets,
         backgroundColor: backgroundColors,
       },
